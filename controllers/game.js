36e3af057377bc868da4613be7e4c12868348181
kirni13 <soujanyakirni13@gmail.com>
2023-04-14 13:55:27 -0500

    routes

--- controllers/game.js
+++ controllers/game.js
@@ -1,25 +1,35 @@
 var game = require('../models/game');
-// List of all Costumes
+// List of all games
 exports.game_list = function(req, res) {
  res.send('NOT IMPLEMENTED: game list');
 };
-// for a specific Costume.
+// for a specific game.
 exports.game_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: game detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
+// Handle game create on POST.
 exports.game_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: game create POST');
 };
-// Handle Costume delete form on DELETE.
-exports.game_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: game delete DELETE ' + req.params.id);
-};
-// Handle Costume update form on PUT.
-exports.game_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: game update PUT' + req.params.id);
-};
-
+// Handle game delete form on DELETE.
+// exports.game_delete = function(req, res) {
+//  res.send('NOT IMPLEMENTED: game delete DELETE ' + req.params.id);
+// };
+// Handle game update form on PUT.
+// exports.game_update_put = function(req, res) {
+//  res.send('NOT IMPLEMENTED: game update PUT' + req.params.id);
+// };
+exports.game_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await game.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // List of all game
 exports.game_list = async function(req, res) {
     try{
@@ -31,6 +41,25 @@ exports.game_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+   exports.game_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await game.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.game_type)
+    toUpdate.gamename = req.body.gamename;
+    if(req.body.playername) toUpdate.playername = req.body.playername;
+    if(req.body.score) toUpdate.score = req.body.score;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 
    // VIEWS
 // Handle a show all view
@@ -44,14 +73,14 @@ exports.game_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
-   // Handle Costume create on POST.
+   // Handle game create on POST.
 exports.game_create_post = async function(req, res) {
     console.log(req.body)
     let document = new game();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"game_type":"goat", "cost":12, "size":"large"}
     document.gamename = req.body.gamename;
     document.playername = req.body.playername;
     document.score = req.body.score;
