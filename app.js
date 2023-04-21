dcd03b734d5711dd975489f4dcbca4c0fab18705
kirni13 <soujanyakirni13@gmail.com>
2023-04-21 12:33:15 -0500

    added account.js

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }
+));
 //Get the default connection
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +40,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 var mongoose = require('mongoose');
@@ -46,6 +73,16 @@ app.use('/game', gameRouter);
 app.use('/Board', boardRouter);
 app.use('/Selector', selectorRouter);
 app.use('/resource', resourceRouter);
+let reseed = false;
+  if (reseed) { recreateDB();}
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -94,6 +131,5 @@ async function recreateDB(){
     console.log('There was an error', e.message);
   });
   }
-  let reseed = false;
-  if (reseed) { recreateDB();}
+  
 module.exports = app;
